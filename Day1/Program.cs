c4beb901ad7223fa86cc7900a639f76ed7b9f038
Nicolas Ding <niding@microsoft.com>
2020-12-01 17:19:38 +0100

    Day1 first commit

---
+++ Day1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = Console.ReadLine();
+            HashSet<int> set = new HashSet<int>();
+            while (!string.IsNullOrEmpty(line))
+            {
+                set.Add(int.Parse(line));
+                line = Console.ReadLine();
+            }
+
+            foreach (int left in set)
+            {
+                foreach (int middle in set)
+                {
+                    if (set.Contains(2020 - left - middle))
+                    {
+                        Console.WriteLine($"{left} x {middle} x {2020 - left - middle} = {left * middle * (2020 - left - middle)}");
+                    }
+                }
+            }
+        }
+    }
+}
