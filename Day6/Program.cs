df3b3bd5a0750c65d5df0d86eb55c19455e1c8cc
Nicolas Ding <niding@microsoft.com>
2020-12-06 11:50:25 +0100

    Day 6

---
+++ Day6/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day6
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int unionCount = 0;
+            int intersectCount = 0;
+            IEnumerable<char> intersectChars;
+            IEnumerable<char> unionChars;
+
+            do
+            {
+                unionChars = new List<char>();
+                intersectChars = Enumerable.Range('a', 26).Select(_ => (char)_).ToList();
+                Utils.Utils.ReadLinesFromConsole()
+                    .ToList()
+                    .ForEach(_ =>
+                    {
+                        unionChars = _.Union(unionChars);
+                        intersectChars = _.Intersect(intersectChars);
+                    });
+                unionCount += unionChars.Count();
+                intersectCount += intersectChars.Count();
+            } while (unionChars.Any());
+
+            Console.WriteLine($"Union : {unionCount}");
+            Console.WriteLine($"Intersect : {intersectCount - 26}");
+        }
+    }
+}
