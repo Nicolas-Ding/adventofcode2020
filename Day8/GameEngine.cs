e14c5afc77d38ca5901e2411f4d3269ff46b2326
Nicolas Ding <niding@microsoft.com>
2020-12-08 10:30:04 +0100

    Day 8

---
+++ Day8/GameEngine.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Day8
+{
+    public class GameEngine
+    {
+        public List<(string instruction, int parameter)> Instructions;
+        public int Accumulator;
+        public int CurrentLine;
+        public GameEngine(List<string> instructions)
+        {
+            Instructions = instructions.Select(_ =>
+            {
+                string[] instruction = _.Split(' ');
+                return (instruction[0], int.Parse(instruction[1]));
+            }).ToList();
+        }
+
+        public bool FindInfiniteLoop()
+        {
+            HashSet<int> visited = new HashSet<int>();
+            Accumulator = 0;
+            CurrentLine = 0;
+            while (!visited.Contains(CurrentLine))
+            {
+                visited.Add(CurrentLine);
+                if (ProcessLine())
+                {
+                    // The program finished, so no infinite loop
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        // returns a boolean if the program is finished
+        public bool ProcessLine()
+        {
+            if (CurrentLine >= Instructions.Count)
+            {
+                return true;
+            }
+
+            var instruction = Instructions[CurrentLine];
+            switch (instruction.instruction)
+            {
+                case "nop":
+                    CurrentLine++;
+                    break;
+                case "acc":
+                    Accumulator += instruction.parameter;
+                    CurrentLine++;
+                    break;
+                case "jmp":
+                    CurrentLine += instruction.parameter;
+                    break;
+            }
+
+            return false;
+        }
+
+        // Changes instruction at lineNumber to newInstruction. No-op if originalInstruction is specified and doesn't match
+        public void ChangeInstruction(int lineNumber, Func<string, string> changeInstruction)
+        {
+            if (lineNumber > Instructions.Count)
+            {
+                return;
+            }
+
+            Instructions[lineNumber] = (changeInstruction(Instructions[lineNumber].instruction), Instructions[lineNumber].parameter);
+        }
+    }
+}
---
+++ Day8/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualBasic.CompilerServices;
+
+namespace Day8
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<string> instructions = Utils.Utils.ReadLinesFromConsole().ToList();
+            GameEngine game = new GameEngine(instructions);
+
+            // Part 1
+            game.FindInfiniteLoop();
+            Console.WriteLine(game.Accumulator);
+
+            // Part 2
+            for (int i = 0; i < instructions.Count; i++)
+            {
+                game.ChangeInstruction(i, InvertNopJmp);
+                if (!game.FindInfiniteLoop())
+                {
+                    Console.WriteLine($"Program terminates when modifying instruction {i}, current Accumulator value is {game.Accumulator}");
+                }
+                game.ChangeInstruction(i, InvertNopJmp);
+            }
+        }
+
+        public static string InvertNopJmp(string instruction)
+        {
+            switch (instruction)
+            {
+                case "nop":
+                    return "jmp";
+                case "jmp":
+                    return "nop";
+                default:
+                    return instruction;
+            }
+        }
+    }
+}
