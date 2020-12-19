3a33675c8b253fe698c504ea58830e82aa0775ce
niding <niding@microsoft.com>
2020-12-19 12:02:39 +0100

    Day 19 Working part 1

---
+++ Day19/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Day19
+{
+    class Program
+    {
+
+        static Regex onlyLetters = new Regex(@"[a-z]+");
+
+        static void Main(string[] args)
+        {
+            Dictionary<int, Rule> rules = Utils.Utils.ReadLinesFromConsole()
+                .Select(_ => _.Split(':'))
+                .ToDictionary(
+                    _ => int.Parse(_[0]),
+                    _ => _[1].Trim().StartsWith('"') ?
+                        new Rule { Letter = _[1].Trim().Trim('"') } : 
+                        new Rule { PossibleRules = _[1].Trim()
+                            .Split('|')
+                            .Select(subRule => subRule.Trim().Split().Select(int.Parse).ToList())
+                            .ToList()}
+                        );
+
+            // Part 2 Special update
+            rules[8] = new Rule
+            {
+                PossibleRules = new List<List<int>>
+                {
+                    new List<int> { 42 },
+                    new List<int> { 42, 8 }
+                }
+            };
+
+            rules[11] = new Rule
+            {
+                PossibleRules = new List<List<int>>
+                {
+                    new List<int> { 42, 31 },
+                    new List<int> { 42, 11, 31 }
+                }
+            };
+
+            //foreach (var kvp in rules)
+            //{
+            //    Console.WriteLine($"{kvp.Key}: {kvp.Value}");
+            //}
+
+            int part1 = Utils.Utils.ReadLinesFromConsole()
+                .Count(line =>
+                {
+                    var res = rules[0].IsMatch(rules, line);
+                    return res.isMatch && res.matchLength == line.Length;
+                });
+
+            Console.WriteLine($"Part 1 : {part1}");
+        }
+    }
+}
---
+++ Day19/Rule.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Day19
+{
+    public class Rule
+    {
+        public string Letter { get; set; }
+
+        public List<List<int>> PossibleRules { get; set; }
+
+        public (bool isMatch, int matchLength) IsMatch(Dictionary<int, Rule> allRules, string input)
+        {
+            var result = IsMatch_Internal(allRules, input);
+            //Console.WriteLine($"Called IsMatch on input = {input} and rule {this}, result was {result.isMatch}");
+            return result;
+        }
+
+
+        public (bool isMatch, int matchLength) IsMatch_Internal(Dictionary<int, Rule> allRules, string input)
+        {
+            if (!string.IsNullOrEmpty(Letter))
+            {
+                return (input.StartsWith(Letter), Letter.Length);
+            }
+
+            int position = 0;
+            Console.WriteLine($"Calling IsMatch on input = {input} and rule {this}. There are {PossibleRules.Count} possible rules");
+            return (PossibleRules.Any(subRule =>
+            {
+                Console.WriteLine($"  testing subrule {string.Join("", subRule)}");
+                position = 0;
+                foreach (int rule in subRule)
+                {
+                    var result = allRules[rule].IsMatch(allRules, input.Substring(position));
+                    Console.WriteLine($"    Called IsMatch on input = {input.Substring(position)}, position = {position} and rule {allRules[rule]}, result was {result.isMatch}");
+                    if (!result.isMatch)
+                    {
+                        return false;
+                    }
+                    position += result.matchLength;
+                }
+                return true;
+            }), position);
+        }
+
+        public override string ToString()
+        {
+            return !string.IsNullOrEmpty(Letter) ? Letter : string.Join('|', PossibleRules.Select(_ => string.Join("", _)));
+        }
+    }
+}
